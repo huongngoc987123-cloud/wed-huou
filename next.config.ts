7bfbc37a732d8ad060d9d145df78802b1dc5566a
vietdev <huongngoc987123@gmail.com>
2026-07-25 23:21:45 -0400

    fix code

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // Allow importing from src/ for a smooth migration
-  },
+  outputFileTracingRoot: __dirname,
   images: {
     remotePatterns: [
       {
